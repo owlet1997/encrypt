7dbca65d911f7b410abe3094132a12a2e60052e7
User <User@WIN-99CJV2MP659>
2021-02-23 20:08:09 +0400

    first lab

---
+++ file.cpp
@@ -0,0 +1,72 @@
+//
+// Created by User on 22.02.2021.
+//
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <random>
+#include <vector>
+#include <ctime>
+#include "file.h"
+
+// Разделить файл на путь и имя файла
+std::vector<std::string> file::SNV_splitFilename(const std::string fileName) {
+    size_t found;
+    found = fileName.find_last_of("/\\");
+    if (found == std::string::npos)
+        return {"", fileName};
+    return {fileName.substr(0, found) + "/", fileName.substr(found + 1)};
+}
+
+std::vector<uint32_t> file::SNV_readFileBinary(std::string fileName) {
+    std::ifstream input(fileName, std::ios::binary | std::ios::in);
+    std::vector<uint32_t> v;
+    if (input.is_open()) {
+        char *buffer = new char[4];
+        while (input.read(buffer, 4)) {
+            uint32_t num;
+            memcpy(&num, buffer, 4);
+            v.push_back(num);
+        }
+        input.close();
+        return v;
+    }
+    std::cerr << "No file found" << std::endl;
+    return {};
+}
+
+//std::vector<uint32_t> file::readWholeFileBinary(std::string fileName) {
+//    std::ifstream input(fileName, std::ios::binary | std::ios::in);
+//    std::vector<uint32_t> v;
+//    if (input.is_open()) {
+//        char *buffer = new char[4];
+//        while (input.read(buffer, 4)) {
+//            uint32_t num;
+//            memcpy(&num, buffer, 4);
+//            v.push_back(num);
+//        }
+//        input.close();
+//        return v;
+//    }
+//    std::cerr << "No file found" << std::endl;
+//    return {};
+//}
+
+bool file::SNV_writeToFile(std::string path, std::vector<uint32_t> v) {
+    std::ofstream output(path);
+    if (output.is_open()) {
+        for (int i = 0; i < 2; i++) {
+            char *buf = new char[4];
+            memcpy(buf, &v[i], 4);
+            for (int j = 0; j < 4; j++) {
+                output << buf[j];
+            }
+        }
+        output.close();
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <ctime>
+#include "file.h"
+
+void genKey(std::string path) {
+    std::mt19937 generator;
+    generator.seed(time(0));
+
+    // Запись в файл
+    std::ofstream output_file(path + "key.txt");
+
+    std::uniform_int_distribution<> distrib(33, 126);
+
+    for (int i = 0; i < 16; i++) {
+        char buffer = (char)distrib(generator);
+            output_file << buffer;
+    }
+    output_file.close();
+}
+
+// Шифрование с помощью алгоритма TEA
+void SNV_encrypt(std::string fileName) {
+    std::cout << "\nStart encryption of file " << fileName << std::endl;
+
+    std::vector<uint32_t> v = file::SNV_readFileBinary(fileName);
+    std::vector<uint32_t> k = file::SNV_readFileBinary(file::SNV_splitFilename(fileName)[0] + "key.txt");
+
+    if (v.empty()){
+        std::cerr << "\nError file reading" << std::endl;
+        return;
+    }
+
+    uint32_t sum = 0;
+    uint32_t delta = 0x9e3779b9;
+
+    for (int i = 0; i < 32; ++i) {
+        sum += delta;
+        v[0] += ((v[1] << 4) + k[0]) ^ (v[1] + sum) ^ ((v[1] >> 5) + k[1]);
+        v[1] += ((v[0] << 4) + k[2]) ^ (v[0] + sum) ^ ((v[0] >> 5) + k[3]);
+    }
+
+    bool flag = file::SNV_writeToFile(fileName + ".enc", v);
+    if (flag){
+        std::cout << "\nResult of encryption had been written to file " << fileName + ".enc" << std::endl;
+    } else {
+        std::cerr << "\nFile opening error" << std::endl;
+    }
+}
+
+// Дешифрование с помощью алгоритма TEA
+void SNV_decrypt(const std::string& fileName, std::string keyFile) {
+
+    std::cout << "\nStart decryption of file " << fileName << std::endl;
+    // Чтение данных для дешифровки
+    std::vector<uint32_t> v = file::SNV_readFileBinary(fileName);
+    if (v.empty()){
+        std::cerr << "\nError file reading" << std::endl;
+        return;
+    }
+
+    // Чтение ключа
+    std::vector<uint32_t> k = file::SNV_readFileBinary(std::move(keyFile));
+
+    // Дешифрование
+    uint32_t sum = 0xC6EF3720;
+    uint32_t delta = 0x9e3779b9;
+
+    for (int i = 0; i < 32; ++i) {
+        v[1] -= ((v[0] << 4) + k[2]) ^ (v[0] + sum) ^ ((v[0] >> 5) + k[3]);
+        v[0] -= ((v[1] << 4) + k[0]) ^ (v[1] + sum) ^ ((v[1] >> 5) + k[1]);
+        sum -= delta;
+    }
+
+    // Определение имени выходного файла
+    std::string initFileName = fileName.substr(0, fileName.find_last_of("."));
+    std::string outputFile = initFileName;
+    int num = 1;
+    while (std::ifstream(outputFile).good()) {
+        outputFile = initFileName;
+        outputFile.insert(outputFile.find_last_of('.'), "(" + std::to_string(num++) + ")");
+    }
+
+    // Запись результатов дешифрования в выходной файл
+    bool flag = file::SNV_writeToFile(outputFile, v);
+    if (flag){
+        std::cout << "\nResult of decryption had been written to file " << outputFile << std::endl;
+    } else {
+        std::cerr << "\nFile opening error" << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 2) {
+        if (strcmp(argv[1], "-e") == 0) {
+            genKey(file::SNV_splitFilename(argv[2])[0]);
+            SNV_encrypt(argv[2]);
+        } else if (strcmp(argv[1], "-d") == 0 && argv[3]) {
+            SNV_decrypt(argv[2], argv[3]);
+        } else {
+            std::cerr << "Wrong parameters" << std::endl;
+            return 1;
+        }
+    } else {
+        genKey("");
+        SNV_encrypt("test.txt");
+    }
+
+    return 0;
+}
